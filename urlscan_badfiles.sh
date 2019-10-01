32c47f492a8bf9d1897806e23f47b35e15c4a858
Lauren P <l.e.proehl@gmail.com>
2019-10-01 17:49:47 -0500

    Updated script to do a better public search
    Thanks to https://twitter.com/thomasksec for pointing out a better way to handle querying for results in the last 24h, adjusted the script to reflect this and make it more efficient. Also adjusted file output types to reflect the change in query language, files will now following URLScanID.ext naming convention.

--- urlscan_badfiles.sh
+++ urlscan_badfiles.sh
@@ -2,9 +2,6 @@
 # Automating the search and download of bad files from public URLScan submissions.
 # Author: Lauren Proehl
 
-# Grabs system date for finding closest API search matches
-DATE=$(date +"%Y-%m-%d")
-
 # Output file for samples to be downloaded
 RESULTS="~/urlscan_badfiles.txt"
 
@@ -22,19 +19,20 @@ EXT=${EXT,,}
 # Example URLScan result search API query
 # https://urlscan.io/api/v1/search/?q=filename:.exe&size=500
 
-# curl the URLScan API for any results with a filename containing the selected extension, up to 1000 search results. (Feel free to change this if you need, I've found around 251 works just as well.) Also output to a holding json file.
-curl -o "~/findings.json" "https://urlscan.io/api/v1/search/?q=filename:."$EXT"&size=1001"
+# curl the URLScan API for any results with a filename containing the selected extension, for the last 24h. Also output to a holding json file.
+curl -o "~/findings.json" "https://urlscan.io/api/v1/search/?q=(filename:."$EXT"%20AND%20page.url:."$EXT")%20date%3A%5Bnow-1d%20TO%20now%5D" 
+
 
 # Take the json file and parse to find any scans that have been marked 'public,' contain the selected file extension, and are within today's date. Then output the URLScan scan results and the original url to the same line, but separate for unique original URLs only.
 cat "~/findings.json" | jq '.results[] | select(.task.visibility =="public") | select (.task.url | contains(".'$EXT'")) | select(.task.time | contains("'$DATE'")) | "\(.result) \(.task.url)"' -r | sort -uk2,2 > "$TMP"
 
-# While reading the URLScan scan URLs and original URLs, curl the URLScan results to see if the scan was 'public,' and if there were any confirmed malicious results. If the scan was both public and malicious, output the original URL and the malicious file SHA256 to the same line in a new file.
+# While reading the URLScan scan URLs and original URLs, curl the URLScan results to see if the scan was 'public,' and if there were any confirmed malicious results. If the scan was both public and malicious, output the original URL and the malicious file URLScan IDs to the same line in a new file.
 while read first second; do
-	curl "$first" | jq 'select(.task.visibility == "public") | select(.verdicts.overall.malicious == true) | "\(.task.url) \(.meta.processors.download.data[].sha256)"' -r >> "$RESULTS"
+	curl "$first" | jq 'select(.task.visibility == "public") | select(.verdicts.overall.malicious == true) | "\(.task.url) \(.data.requests[].request.requestId)"' -r >> "$RESULTS"
 	sleep 3s
 done < "$TMP"
 
-# While reading all confirmed malicious URLs and SHA256, wget the original URL to try to download the malicious file. If first connection succesful, output the file name as the SHA256 paired with the selected extension. If first connection is not succesful, attempt connection up to 5 times, with a 3 second wait in between each retry attempt. 
+# While reading all confirmed malicious URLs and URLScan IDs, wget the original URL to try to download the malicious file. If first connection succesful, output the file name as the URLScan IDs paired with the selected extension. If first connection is not succesful, attempt connection up to 5 times, with a 3 second wait in between each retry attempt. 
 # Also leverages the '--retry-connrefused' flag in an attempt to catch intermittently unavailable websites.
 while read first second; do
 	wget -O "$second"."$EXT" --tries=5  --waitretry=3 --retry-connrefused "$first"
